3b994e2c9e3464f3b794b01c7c9bb468f0aaeee7
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2022-02-11 12:58:16 +0100

    Refactoring: Destruct weather data

--- src/components/WeatherEntry.tsx
+++ src/components/WeatherEntry.tsx
@@ -5,35 +5,35 @@ import { getIconUrl } from '../services/WeatherService';
 interface WeatherEntryProps {
   weather: Weather;
 }
+
 // review: Good job in terms of defining type however for more consistency
 // you can use function expression everywhere like you used for defining component
 const convertUnixTimeToDate = (unixUtc: number): Date => {
   return new Date(unixUtc * 1000);
 };
 
-export const WeatherEntry = ({ weather }: WeatherEntryProps) => (
+export const WeatherEntry = ({weather}: WeatherEntryProps) => {
   // review: you can also destruct "weather" to make it shorter on usage
   // also "weather.weather" on line 32 does not make sense, could be like below:
-  // const { weather, main: { temp, humidity, temp_max }, dt} = weather;
+  const { weather: weatherConditions , main: { temp, humidity, temp_max, temp_min}, dt: date} = weather;
 
-  <div>
-    <div>{convertUnixTimeToDate(weather.dt).toLocaleTimeString()}</div>
+  return <div>
+    <div>{convertUnixTimeToDate(date).toLocaleTimeString()}</div>
 
     <div>
-      <strong>{weather.main.temp}°C</strong>
+      <strong>{temp}°C</strong>
 
       <div>
-        ({weather.main.temp_min}°C / {weather.main.temp_max}°C)
+        ({temp_min}°C / {temp_max}°C)
       </div>
     </div>
 
-    <div>Humidity: {weather.main.humidity}%</div>
+    <div>Humidity: {humidity}%</div>
 
-    {weather.weather.map((condition: WeatherConditions) => (
+    {weatherConditions.map((condition: WeatherConditions) => (
       <div key={condition.id}>
-        <img src={getIconUrl(condition.icon)} alt={condition.main} />{' '}
-        {condition.main} {condition.description}
+        <img src={getIconUrl(condition.icon)} alt={condition.main} />{' '} {condition.main} {condition.description}
       </div>
     ))}
   </div>
-);
+}
