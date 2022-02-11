53312e7975b3db4eefbcbbe2dc62f8cdf15af2ac
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2022-02-11 12:50:49 +0100

    Refactoring
         # Get rid of if/else
         # fragment
         # FC type

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ import { WeatherSummery } from './components/WeatherSummery';
 import { WeatherLocation } from './model/Weather';
 import WeatherService from './services/WeatherService';
 
+// TODO: since FC is gonna be remove from the nest version of the react it is better to do not use it anymore, here is the explanation:
+// React.FC is unnecessary: it provides next to no benefits and has a few downsides. (See below.)
+// I see a lot of beginners to TS+React using it, however, and I think that it's usage in this template is a contributing factor to that,
+// as the prominence of this template makes it a de facto source of "best practice".
+// https://github.com/facebook/create-react-app/pull/8177
 const App: FC = () => {
-  const localLocation = localStorage.getItem('location')
+  const localLocation: WeatherLocation[] = localStorage.getItem('location')
     ? JSON.parse(localStorage.getItem('location'))
     : [];
   const [locations, setLocations] = useState<WeatherLocation[]>(localLocation);
@@ -19,6 +24,21 @@ const App: FC = () => {
     try {
       resetAlert();
       const location = (await WeatherService.searchLocation(term)).data;
+
+      //TODO: you can also get rid of "else" here by breaking the code at that point
+      /*
+        if (!location) {
+          return setError(`No location found called ${term}`);
+        }
+
+        if (locations.find((item) => item.id === location.id)) {
+          return setWarning(`Location '${term}' is already in the list.`);
+        }
+
+        setLocations([location, ...locations]);
+     */
+
+
       if (!location) {
         setError(`No location found called ${term}`);
       } else if (locations.find((item) => item.id === location.id)) {
